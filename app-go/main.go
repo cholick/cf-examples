bbbe6265e02fc1f66209370555b45b826a6e71ff
Matt Cholick <cholick@gmail.com>
2017-11-07 07:27:14 -0800

    Cleanup golang app

--- app-go/main.go
+++ app-go/main.go
@@ -5,22 +5,43 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 )
 
-func main() {
-	http.HandleFunc("/", index)
+type panicHandler struct {
+	http.Handler
+}
 
-	port := getPort()
+func (h panicHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer handleFatalError()
+	h.Handler.ServeHTTP(w, r)
+}
 
-	fmt.Println("Listening on port [" + port + "]")
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal(err)
+func handleFatalError() {
+	e := recover()
+	if e == nil {
+		return
 	}
+
+	stack := make([]byte, 1<<16)
+	stackSize := runtime.Stack(stack, true)
+	prettyStack := string(stack[:stackSize])
+
+	fmt.Fprintf(os.Stderr, "panic: %v", e)
+	fmt.Fprintf(os.Stderr, prettyStack)
+	os.Exit(1)
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "Success: Go")
+	fmt.Fprintf(res, "Success: Go.")
+}
+
+func notFound(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(404)
+}
+
+func crashHandler(res http.ResponseWriter, req *http.Request) {
+	panic("oh no!")
 }
 
 func getPort() string {
@@ -29,4 +50,18 @@ func getPort() string {
 		port = "3000"
 	}
 	return port
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/favicon.ico", notFound)
+	http.Handle("/crash", panicHandler{http.HandlerFunc(crashHandler)})
+
+	port := getPort()
+
+	fmt.Println("Listening on port [" + port + "]")
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
